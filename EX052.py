f71c2f8a727f9cff9bc096aad9f06b5e02fd9f14
MiiyamotoMusashi <120853589+MiiyamotoMusashi@users.noreply.github.com>
2023-01-24 10:42:51 -0300

    Add files via upload

---
+++ EX052.py
@@ -0,0 +1,10 @@
+def calcular():
+    for i in range(5):
+        n = int(input('Número: '))
+        
+    for a in range(1, n+1):
+        divisão = n / a
+        print(a)
+        print(divisão)
+        
+calcular()
