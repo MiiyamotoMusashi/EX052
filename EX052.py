504b721fbc3a640e434ad3ce96e79425846a5516
Luiz Fernando Schmitt Filho <120853589+MiiyamotoMusashi@users.noreply.github.com>
2023-02-05 22:46:55 -0300

    Update EX052.py

--- EX052.py
+++ EX052.py
@@ -1,10 +1,16 @@
-def calcular():
-    for i in range(5):
-        n = int(input('Número: '))
-        
-    for a in range(1, n+1):
-        divisão = n / a
-        print(a)
-        print(divisão)
-        
-calcular()
+n = int(input('Digíte um número: '))
+c = 0
+
+for i in range(1, n + 1):
+    if n % i == 0:
+        c += 1
+        print("\033[32m", end='')
+    else:
+        print("\033[31m", end='')
+
+    print(f'{i} ', end='')
+
+if c == 2:
+    print('\n\n\033[mEsse número foi divisível 2 vezes então ele é primo')
+else:
+    print(f'\n\n\033[mEsse número foi dividido {c} ves(zes) então ele não é primo')
